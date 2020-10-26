901b4197c8efa87cbeda4c06cc21cf7edc02e5b6
Paul Morken <pmorken@ti.com>
2020-10-25 18:55:19 -0600

    Fixed top nav not hiding

--- scripts/navbar-scroll.js
+++ scripts/navbar-scroll.js
@@ -1,6 +1,6 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.addEventListener('scroll', function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").classList.remove("scroll-up");
@@ -9,4 +9,4 @@ window.onscroll = function() {
     document.getElementById("navbar").classList.add("scroll-up");
   }
   prevScrollpos = currentScrollPos;
-}
+});
